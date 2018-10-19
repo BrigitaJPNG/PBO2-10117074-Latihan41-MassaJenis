2d807e75e5eb9f48cadfd1a5aa8ff59dcad8e95d
MY PC <MY PC@HOME-UEFI1TB.local>
2018-10-20 03:53:49 +0700

    Project Brigita Julia 10117074 PBO2

---
+++ src/pbo2/pkg10117074/latihan41/massajenis/Kubus.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117074.latihan41.massajenis;
+
+public class Kubus {
+    private int sisi;
+    private int massa;
+
+    public int getSisi() {
+        return sisi;
+    }
+
+    public void setSisi(int sisi) {
+        this.sisi = sisi;
+    }
+
+    public int getMassa() {
+        return massa;
+    }
+
+    public void setMassa(int massa) {
+        this.massa = massa;
+    }
+
+    public int hitungVolume (int parSisi){
+        int hitungVolume = sisi * sisi * sisi;
+        return hitungVolume;
+    }
+    
+    public int hitungMassaJenis (int parMassa, int volume){
+        int hitungMassaJenis = volume / massa;
+        return hitungMassaJenis;
+    }
+}
